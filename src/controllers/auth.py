0d8f7c508e8f587661833e4936bdf9c4c052771d
AndiRaihan <dzaky.raihan@gmail.com>
2024-07-27 17:03:04 +0700

    FEAT: Started on vendor auth

--- src/controllers/auth.py
+++ src/controllers/auth.py
@@ -3,8 +3,8 @@ from fastapi_jwt_auth import AuthJWT
 from sqlalchemy.orm import Session
 
 from src.database import get_db
-from src.schemas.auth import UserCreate, UserLogin
-from src.services.auth import register_user_service, login_user_service
+from src.schemas.auth import UserCreate, UserLogin, StreetVendorCreate
+from src.services.auth import register_user_service, login_user_service, register_vendor_service
 
 auth_router = APIRouter(prefix="/api/v1/auth")
 
@@ -17,3 +17,8 @@ def register_user(user: UserCreate, db: Session = Depends(get_db)):
 @auth_router.post("/login")
 def login_user(user: UserLogin, db: Session = Depends(get_db), authorize: AuthJWT = Depends()):
     return login_user_service(user, db, authorize)
+
+
+@auth_router.post("/register-vendor")
+def register_vendor(vendor: StreetVendorCreate, db: Session = Depends(get_db)):
+    return register_vendor_service(vendor, db)
--- src/services/auth.py
+++ src/services/auth.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 from starlette.responses import JSONResponse
 
 from src.models.auth import User
-from src.schemas.auth import UserCreate, UserLogin
+from src.schemas.auth import UserCreate, UserLogin, StreetVendorCreate
 from src.utils.auth import hash_password, create_access_token, verify_password, get_user, get_user_by_email
 
 
@@ -21,6 +21,13 @@ def register_user_service(user: UserCreate, db: Session) -> Response:
     db.refresh(user_instance)
     # TODO: Make a DTO for the response
     return JSONResponse(status_code=201, content={"id": user_instance.id, "email": user_instance.email})
+
+
+def register_vendor_service(vendor: StreetVendorCreate, db: Session) -> Response:
+    if get_user_by_email(vendor.email, db):
+        return JSONResponse(status_code=409, content={"message": "User already exists"})
+    user_attributes = vendor.dict(exclude={"street_vendor_name", "street_vendor_category"})
+    return JSONResponse(status_code=201, content={"message": user_attributes})
 
 
 def login_user_service(user: UserLogin, db: Session, authorize: AuthJWT) -> JSONResponse | Any:
