4a08ebd3c296af084823927c8157e0c3ace92024
AndiRaihan <dzaky.raihan@gmail.com>
2024-07-27 21:00:13 +0700

    FEAT: Added delete and update to items

--- src/controllers/item.py
+++ src/controllers/item.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import Session
 
 from src.database import get_db
 from src.schemas.item import ItemCreateRequest, ItemUpdateRequest
-from src.services.item import create_items_service, update_items_service
+from src.services.item import create_items_service, update_items_service, get_items_by_vendor_service, \
+    delete_items_batch_service
 
 items_router = APIRouter(prefix="/api/v1/items")
 
@@ -16,3 +17,13 @@ def create_items(items_create_request: ItemCreateRequest, db: Session = Depends(
 @items_router.put("/update")
 def update_items(items_create_request: ItemUpdateRequest, db: Session = Depends(get_db)):
     return update_items_service(items_create_request, db)
+
+
+@items_router.get("/get-by-vendor/{vendor_id}")
+def get_items_by_vendor(vendor_id: int, db: Session = Depends(get_db)):
+    return get_items_by_vendor_service(vendor_id, db)
+
+
+@items_router.delete("/delete")
+def delete_items_batch(items: list[int], db: Session = Depends(get_db)):
+    return delete_items_batch_service(items, db)
--- src/services/item.py
+++ src/services/item.py
@@ -35,3 +35,21 @@ def update_items_service(items: ItemUpdateRequest, db: Session):
     except SQLAlchemyError as e:
         db.rollback()  # Rollback the transaction on error
         raise e  # Optionally, re-raise the exception or handle it as needed
+
+
+def get_items_by_vendor_service(vendor_id: int, db: Session):
+    return db.query(Item).filter(Item.street_vendor == vendor_id).all()
+
+
+def delete_items_batch_service(items: list[int], db: Session):
+    try:
+        # Start a transaction
+        with db.begin():
+            db.query(Item).filter(Item.id.in_(items)).delete(synchronize_session=False)
+            db.commit()  # Commit the transaction
+
+        return items
+
+    except SQLAlchemyError as e:
+        db.rollback()  # Rollback the transaction on error
+        raise e  # Optionally, re-raise the exception or handle it as needed
